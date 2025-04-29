5952ccc124fedc240419813feea81394a9a73159
sanvie <sanvie25.tech@gmail.com>
2025-04-29 09:34:21 +0530

    added function

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ document.getElementById('search-box').addEventListener('input', async (e) => {
 	  }
 	}
   });
+  function displayResults(books) {
+	const resultContainer = document.getElementById('result-container');
+	resultContainer.innerHTML = books.map(book => {
+	  const title = book.volumeInfo.title || "No Title";
+	  const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : "No Authors";
+	  const thumbnail = book.volumeInfo.imageLinks?.thumbnail || "https://via.placeholder.com/150";
+	  const bookId = book.id;
+
+	  return `
+		<div class="book-result" data-id="${bookId}">
+		  <img src="${thumbnail}" alt="${title}">
+		  <h3>${title}</h3>
+		  <p>${authors}</p>
+		</div>`;
+	}).join('');
