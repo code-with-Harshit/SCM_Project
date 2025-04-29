2cef457330f540cb8dfc006078663baa1632a3bb
sanvie <sanvie25.tech@gmail.com>
2025-04-29 09:33:07 +0530

    started adding js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('search-box').addEventListener('input', async (e) => {
+	const query = e.target.value.trim();
+	if (query.length > 2) {
+	  try {
+		const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`);
+		const data = await response.json();
+		displayResults(data.items || []);
+	  } catch (error) {
+		console.error("Error fetching data:", error);
+	  }
+	}
+  });
